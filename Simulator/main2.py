aed4e5162a14ad5fb41167c1d78634f9810fb109
Aadit Patel <aadit.patel@tugoticonsulting.com>
2013-11-14 13:08:35 -0800

    Added basic sekelton

---
+++ Simulator/main2.py
@@ -0,0 +1,27 @@
+# Sim 2
+# Given a target on the map, have the animat go to it
+# When it reaches the target, place a new target somewhere, repeat
+
+import matplotlib
+matplotlib.use('TKAgg')
+from Env import Env
+from Animat import Animat
+import time
+from numpy import zeros
+import random
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+print 'Running Simulation 2'
+mapSize = 50
+food = Env(mapSize)
+a = Animat.randomStart(mapSize,mapSize)
+
+for iteration in range(1,10):
+	# Pick a random spot
+	foody = random.randrange(1,mapSize+1)
+	foodx = random.randrange(1,mapSize+1)
+	food.map[foody,foodx] = 5 # random number
+	
+	
+	
