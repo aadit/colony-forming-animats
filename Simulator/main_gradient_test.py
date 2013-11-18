5d0c5dc51a509b7b93d3ebf66be64f217f83d27e
Lawrence Chang <chang@badassimac.local>
2013-11-17 19:18:48 -0800

    Gradient function seems to work

--- Simulator/main_gradient_test.py
+++ Simulator/main_gradient_test.py
@@ -4,3 +4,33 @@
 # "add" the appropriate gradient to our environment centered on the food
 # Be wary of even and odd numbers of columns, rows
 
+import matplotlib
+matplotlib.use('TKAgg')
+import sys
+sys.path.append("..")
+
+from Environment.Env import Env
+from Animat.Animat import Animat
+import time
+from numpy import zeros
+import random
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+print 'Running Simulation 3 - Gradient maker'
+mapSize = 103
+food = Env(mapSize)
+a = Animat.randomStart(mapSize,mapSize)
+food.makeGradient()
+
+
+fig1 = plt.figure()
+plt.pcolor(food.gradient)
+plt.ion()
+plt.show()
+
+food.makeFoodRandom()
+fig2 = plt.figure()
+plt.pcolor(food.map)
+plt.ion()
+plt.show()
