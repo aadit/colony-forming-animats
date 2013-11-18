efc5079772118bf63b7112dfa43dcc80e30c446c
Lawrence Chang <chang@badassimac.local>
2013-11-17 17:53:14 -0800

    Adding simulator for gradients, no code yet

---
+++ Simulator/main_gradient_test.py
@@ -0,0 +1,6 @@
+# Sim 3
+# Make a gradient map four times as big as our environment map
+# Place a "food" item somewhere in our environment
+# "add" the appropriate gradient to our environment centered on the food
+# Be wary of even and odd numbers of columns, rows
+
